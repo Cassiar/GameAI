8030be72310b69f41e367124fe27895b2cab3feb
Cassiar <cdb7951@rit.edu>
2022-10-21 17:47:23 -0400

    added make fire action

--- GameArchitecture/Assets/Scripts/AMakeFire.cs
+++ GameArchitecture/Assets/Scripts/AMakeFire.cs
@@ -25,9 +25,50 @@ public class AMakeFire : GOAPAction
     /// <exception cref="System.NotImplementedException"></exception>
     public override bool Run(Agent agent)
     {
-        Instantiate(fire);
-        fire.transform.position = agent.transform.position;
-        agent.inventory.Remove("kindling");
-        return true;
+        //need kindling to make fire
+        if (!agent.inventory.Contains("kindling"))
+        {
+            //action is over because we can't complete it
+            return true;
+        }
+
+        Vector3 agPos = agent.transform.position;
+        //if the agent is already targeted toward a camp fire
+        //then we check the distance, to see if we can make fire or need to move
+        if (agent.target != null && agent.target.tag == "CampFire")
+        {
+            if (Vector3.Distance(agPos, agent.target.transform.position) <= 1)
+            {
+                Instantiate(fire);
+                fire.transform.position = agent.transform.position;
+                agent.inventory.Remove("kindling");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        float closetDist = int.MaxValue;
+        int closestIndex = -1;
+        //get all instances of kindling piles
+        GameObject[] campFires = GameObject.FindGameObjectsWithTag("CampFire");
+        for (int i = 0; i < campFires.Length; i++)
+        {
+            float dist = Vector3.Distance(agPos, campFires[i].transform.position);
+            if (dist < closetDist)
+            {
+                closetDist = dist;
+                closestIndex = i;
+            }
+
+        }
+
+        //return false because the action isn't over
+        agent.target = campFires[closestIndex];
+        return false;
+
+
     }
 }
