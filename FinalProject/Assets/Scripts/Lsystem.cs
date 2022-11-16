77a4bfa62a7035d58c49efe4df25b674bdb867c1
Cassiar <cdb7951@rit.edu>
2022-11-16 11:53:58 -0500

    objects are properly placed next to eachother

--- FinalProject/Assets/Scripts/Lsystem.cs
+++ FinalProject/Assets/Scripts/Lsystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.TerrainTools;
 using UnityEngine;
 
 public class Lsystem : MonoBehaviour
@@ -10,7 +11,7 @@ public class Lsystem : MonoBehaviour
     // + - add 90 degrees to rotation
     // - - subtraction 90 from rotation
     // | - draw path
-    // 
+    // # - draw a room
     //map different characters to what rules they're replaced wit
     [SerializeField]
     private Dictionary<char, string> rules = new Dictionary<char, string>();
@@ -29,12 +30,22 @@ public class Lsystem : MonoBehaviour
     //hold position and rotation when drawing dungeon
     private Stack<Vector3> positions = new Stack<Vector3>();
     private Stack<Vector3> rotations = new Stack<Vector3>();
+    private Stack<float> prevLengths = new Stack<float>();
+
+    private Vector3 pos = new Vector3(0, 0, 0);
+    private Vector3 rot = new Vector3(0, 0, 0);
+
+    //list of objects that will be instantiated
+    [SerializeField]
+    private List<GameObject> terrain = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
         //create rules
-        rules.Add('|', "||[+|][-|]");
+        rules.Add('|', "||#[+|][-|]");
+        //rules.Add('#', "#[[+#]-#]");
+        //rules.Add('#', "#[[+#]-#]#[[+#]-#]#[[+#]-#]");
         //loop for each iteration
         for (int i = 0; i < numIterations; i++)
         {
@@ -64,10 +75,65 @@ public class Lsystem : MonoBehaviour
             backBuffer.Clear();
         }
 
+        string test = "";
         //print the final string for testing
         for (int i = 0; i < buffer.Count; i++)
         {
-            Debug.Log(buffer[i] + ", ");
+            test += buffer[i] + ", ";
+        }
+
+        Debug.Log(test);
+
+        float prevLength = 0;
+        float length = 0;
+        //draw the objects
+        for(int i = 0; i < buffer.Count; i++)
+        {
+            switch (buffer[i])
+            {
+                case '|':
+                    //move forward equal to half the length of the previous object
+                    //since origin is in center of model
+                    //multiplying by rotation to face correct direction
+                    pos += Quaternion.Euler(rot) * new Vector3(prevLength, 0, 0);
+                    //update length to be for this object
+                    length = terrain[0].transform.localScale.x / 2;
+                    //then move forward half again so we're half dist
+                    //away from end of previous
+                    pos += Quaternion.Euler(rot) * new Vector3(length, 0, 0);
+                    Instantiate(terrain[0], pos, Quaternion.Euler(rot));
+                    prevLength = length;
+                    break;
+                case '#':                    
+                    //move forward equal to half the length of the previous object
+                    //since origin is in center of model
+                    //multiplying by rotation to face correct direction
+                    pos += Quaternion.Euler(rot) * new Vector3(prevLength, 0, 0);
+                    //update length to be for this object
+                    length = terrain[1].transform.localScale.x / 2;
+                    //then move forward half again so we're half dist
+                    //away from end of previous
+                    pos += Quaternion.Euler(rot) * new Vector3(length, 0, 0);
+                    Instantiate(terrain[1], pos, Quaternion.Euler(rot));
+                    prevLength = length;
+                    break;
+                case '[':
+                    positions.Push(pos);
+                    rotations.Push(rot);
+                    prevLengths.Push(prevLength);
+                    break;
+                case ']':
+                    pos = positions.Pop();
+                    rot = rotations.Pop();
+                    prevLength = prevLengths.Pop();
+                    break;
+                case '+':
+                    rot.y += 90;
+                    break;
+                case '-':
+                    rot.y -= 90;
+                    break;
+            }
         }
     }
 
